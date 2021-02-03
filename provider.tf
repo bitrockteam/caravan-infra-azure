edd22d9bbaf2eb76912f05c4a58a35330ca2984b
Simone Ripamonti <simone.ripamonti@bitrock.it>
2021-02-03 10:39:57 +0100

    Network and SG

--- provider.tf
+++ provider.tf
@@ -1,4 +1,5 @@
 provider "azurerm" {
   version = "~> 2.45.1"
   features {}
+  skip_provider_registration = true
 }
--- variables.tf
+++ variables.tf
@@ -7,3 +7,23 @@ variable "prefix" {
 variable "location" {
   type = string
 }
+
+variable "tags" {
+  type = map(string)
+  default = {
+    project = "caravan"
+  }
+}
+
+variable "vnet_cidrs" {
+  type = list(string)
+  default = ["10.0.0.0/16"]
+}
+variable "subnet_cidr" {
+  type = string
+  default = "10.0.1.0/24"
+}
+variable "allowed_ssh_cidrs" {
+  type = list(string)
+  default = ["0.0.0.0/0"]
+}
