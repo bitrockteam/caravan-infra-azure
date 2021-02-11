5f9bf8f6d714227de622bb41bf3e99d5084ac55c
Simone Ripamonti <simone.ripamonti@bitrock.it>
2021-02-11 13:03:11 +0100

    Change branch for caravan-acme-le

--- certs.tf
+++ certs.tf
@@ -9,7 +9,7 @@ provider "acme" {
 
 module "terraform_acme_le" {
   depends_on            = [azurerm_dns_ns_record.this]
-  source                = "git::ssh://git@github.com/bitrockteam/caravan-acme-le?ref=feature/support-p12"
+  source                = "git::ssh://git@github.com/bitrockteam/caravan-acme-le?ref=master"
   common_name           = "${var.prefix}.${var.external_domain}"
   dns_provider          = "azure"
   private_key           = tls_private_key.cert_private_key.private_key_pem
